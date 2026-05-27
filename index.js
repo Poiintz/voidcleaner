2f6510845e9f731dcab588a708aacffba935ae59
Poiintz <jedhernandez12345@gmail.com>
2026-05-26 23:15:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('pong');
+    }
+});
+
 client.login(process.env.token);
